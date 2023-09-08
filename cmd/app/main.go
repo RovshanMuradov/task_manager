78cf098470cb8b468b6f819b469d0b05a28e88ab
Rovshan <rovshan2525@gmail.com>
2023-09-08 22:42:03 +0400

    complete project

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"myproject/Cgpt/internal"
+	"time"
+)
+
 func main() {
 
+	// экземпляр проекта
+	project := internal.NewProject()
+
+	// задачи, которые добавляем в проект
+	task1 := internal.Task{
+		ID:          1,
+		Title:       "Задача 1",
+		Description: "Описание задачи 1",
+		Status:      internal.StatusPending,
+		DueDate:     time.Now().AddDate(0, 0, 7), // Дедлайн через 7 дней
+	}
+
+	task2 := internal.Task{
+		ID:          2,
+		Title:       "Задача 2",
+		Description: "Описание задачи 2 ",
+		Status:      internal.StatusInProgress,
+		DueDate:     time.Now().AddDate(0, 0, 14),
+	}
+
+	project.AddTask(task1)
+	project.AddTask(task2)
+
+	// Выводим список задач
+	tasks := project.ListTasks()
+	fmt.Println("Список задач:")
+	for _, task := range tasks {
+		fmt.Printf("ID: %d, Заголовок: %s, Статус: %v\n", task.ID, task.Title, internal.TaskStatusStrings[task.Status])
+	}
+
+	// Изменяем статус задачи
+	err := project.UpdateTaskStatus(1, internal.StatusCompleted)
+	if err != nil {
+		fmt.Println("Ошибка при изменении статуса задачи:", err)
+	} else {
+		fmt.Println("Статус задачи изменен.")
+	}
+
+	// Выводим обновленный список задач
+	tasks = project.ListTasks()
+	fmt.Println("Обновленный список задач:")
+	for _, task := range tasks {
+		fmt.Printf("ID: %d, Заголовок: %s, Статус %v\n", task.ID, task.Title, internal.TaskStatusStrings[task.Status])
+	}
 }
--- internal/task.go
+++ internal/task.go
@@ -1,6 +1,9 @@
 package internal
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type TaskStatus int
 
@@ -10,6 +13,13 @@ const (
 	StatusCompleted
 	StatusCanceled
 )
+
+var TaskStatusStrings = [...]string{
+	StatusPending:    "Ожидает",
+	StatusInProgress: "В процессе",
+	StatusCompleted:  "Завершена",
+	StatusCanceled:   "Отменена",
+}
 
 type Task struct {
 	ID          int
@@ -21,6 +31,10 @@ type Task struct {
 
 type Project struct {
 	Tasks []Task
+}
+
+func NewProject() *Project {
+	return &Project{}
 }
 
 // AddTask TODO: добавляет новую задачу в проект
@@ -41,11 +55,17 @@ func (p *Project) RemoveTask(taskID int) {
 }
 
 // UpdateTaskStatus TODO: изменяет статус задачи по заданному идентификатору
-func (p *Project) UpdateTaskStatus(taskID int, status string) {
-
+func (p *Project) UpdateTaskStatus(taskID int, status TaskStatus) error {
+	for i, task := range p.Tasks {
+		if task.ID == taskID {
+			p.Tasks[i].Status = status
+			return nil
+		}
+	}
+	return fmt.Errorf("task with ID %d not found", taskID)
 }
 
 // ListTasks TODO: выводит список всех задач в проекте
-func (p *Project) ListTasks() {
-
+func (p *Project) ListTasks() []Task {
+	return p.Tasks
 }
